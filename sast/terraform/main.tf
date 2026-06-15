d9fbf2a76fac8f066a32d28530727a56debe9865
Spicehandler <jtspicehandler@gmail.com>
2026-06-15 16:55:23 -0700

    Update main.tf
    removed ssh keypair in sast backend terraform main.tf

--- sast/terraform/main.tf
+++ sast/terraform/main.tf
@@ -83,7 +83,6 @@ resource "aws_instance" "sast_server" {
   vpc_security_group_ids      = [aws_security_group.sast_sg.id]
   associate_public_ip_address = true
   iam_instance_profile        = "LabInstanceProfile"
-  key_name                    = "vockey"
   user_data_replace_on_change = true
 
   user_data = <<EOF
